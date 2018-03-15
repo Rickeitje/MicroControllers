b709e08b1b40a44977e8984e4a43c662677699a6
Unknown <kevinquist@zeelandnet.nl>
2018-03-15 15:49:19 +0100

    B1/B2

--- Opdrachten/Week4/Week4/Week4/main.c
+++ Opdrachten/Week4/Week4/Week4/main.c
@@ -1,58 +1,58 @@
-/*
- * Project name:
-     Demo4_1 : Free running AD conversion on channel 1, PORTF.1
- * Author: Avans-TI, JW
- * Revision History: 
-     20101229: - initial release;
- * Description:
-     This program gives an interrupt on each ms
- * Test configuration:
-     MCU:             ATmega128
-     Dev.Board:       BIGAVR6
-     Oscillator:      External Clock 08.0000 MHz
-     Ext. Modules:    -
-     SW:              AVR-GCC
- * NOTES:
-     - Turn ON the PORT LEDs at SW12.1 - SW12.8
-*/
+
+#define F_CPU 8000000
 
 #include <avr/io.h>
 #include <util/delay.h>
 #include <avr/interrupt.h>
 
-#define BIT(x)	(1 << (x))
+typedef unsigned long int u_int32;
+typedef unsigned int u_int16;
 
-// wait(): busy waiting for 'ms' millisecond
-// Used library: util/delay.h
-void wait( int ms )
+u_int16 measured = 0;
+
+ISR(ADC_vect)
 {
-	for (int tms=0; tms<ms; tms++)
-	{
-		_delay_ms( 1 );			// library function (max 30 ms at 8MHz)
-	}
+	u_int32 result = ADC;
+	result *= 5000;
+	result /= 1023;
+	measured = result;
 }
 
-
-// Initialize ADC: 10-bits (left justified), free running
 void adcInit( void )
 {
-	ADMUX = 0b01100001;			// AREF=VCC, result left adjusted, channel1 at pin PF1
-	ADCSRA = 0b11100110;		// ADC-enable, no interrupt, start, free running, division by 64
+	ADMUX = 0x01;
+	ADCSRA = 0xEE;
 }
 
-
-// Main program: ADC at PF1
-int main( void )
+int main(void)
 {
-	DDRF = 0x00;				// set PORTF for input (ADC)
-	DDRA = 0xFF;				// set PORTA for output 
-	DDRB = 0xFF;				// set PORTB for output
-	adcInit();					// initialize ADC
-
+	DDRA = 0xFF;
+	DDRB = 0xFF;
+	DDRF = 0x00;
+	
+	adcInit();
+	
+	sei();
+	
 	while (1)
 	{
-		PORTB = ADCL;			// Show MSB/LSB (bit 10:0) of ADC
-		PORTA = ADCH;
-		wait(100);				// every 100 ms (busy waiting)
+		u_int32 lamps = measured / 625 ;
+		
+		if(lamps<=1)
+		PORTA = PORTB = 0b10000000;
+		else if(lamps <= 2)
+		PORTA = PORTB = 0b11000000;
+		else if(lamps <= 3)
+		PORTA = PORTB = 0b11100000;
+		else if(lamps <= 4)
+		PORTA = PORTB = 0b11110000;
+		else if(lamps <= 5)
+		PORTA = PORTB = 0b111111000;
+		else if(lamps <= 6)
+		PORTA = PORTB = 0b111111100;
+		else if(lamps <= 7)
+		PORTA = PORTB = 0b111111110;
+		else
+		PORTA = PORTB = 0b111111111;		
 	}
 }
